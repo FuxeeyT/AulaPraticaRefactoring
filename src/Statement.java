83d24b1865b11804ba4d17b9e048ccd57a166bf7
FuxeeyT <anthonycaetano56@gmail.com>
2024-11-26 22:38:16 -0300

    Commit 17

--- src/Statement.java
+++ src/Statement.java
@@ -1,5 +1,22 @@
 package AulaPraticaRefactoring.src;
 
+import java.util.Enumeration;
+
 public abstract class Statement {
-    public abstract String value(Customer customer);
+    public String value(Customer customer) {
+        Enumeration rentals = customer.getRentals();
+        String result = headerString(customer.getName());
+        
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += rentalString(each);
+        }
+        
+        result += footerString(customer.getTotalCharge(), customer.getTotalFrequentRenterPoints());
+        return result;
+    }
+
+    abstract String headerString(String customerName);
+    abstract String rentalString(Rental rental);
+    abstract String footerString(double totalCharge, int frequentRenterPoints);
 }
--- src/TextStatement.java
+++ src/TextStatement.java
@@ -1,23 +1,21 @@
 package AulaPraticaRefactoring.src;
 
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() + "\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
-        }
-        //add footer lines
-        result += "Amount owed is " +
-                String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += "You earned " +
-                String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-                " frequent renter points";
-        return result;
+    @Override
+    String headerString(String customerName) {
+        return "Rental Record for " + customerName + "\n";
+    }
+
+    @Override
+    String rentalString(Rental rental) {
+        return "\t" + rental.getMovie().getTitle() + "\t" + 
+               String.valueOf(rental.getCharge()) + "\n";
+    }
+
+    @Override
+    String footerString(double totalCharge, int frequentRenterPoints) {
+        return "Amount owed is " + String.valueOf(totalCharge) + "\n" +
+               "You earned " + String.valueOf(frequentRenterPoints) +
+               " frequent renter points";
     }
 }

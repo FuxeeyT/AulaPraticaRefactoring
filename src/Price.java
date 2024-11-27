a003ebd2217442fd4b5e2a91160f9cb9094e4cca
FuxeeyT <anthonycaetano56@gmail.com>
2024-11-26 22:39:56 -0300

    Commit 18

--- src/Price.java
+++ src/Price.java
@@ -1,5 +1,5 @@
 package AulaPraticaRefactoring.src;
-// não alterei nada pq fiz no commit 14
+
 public abstract class Price {
    public abstract int getPriceCode();
    public abstract double getCharge(int daysRented);

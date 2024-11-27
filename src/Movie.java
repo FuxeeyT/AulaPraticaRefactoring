a1b1793d5c374ca596c1746338730c93e03e6298
FuxeeyT <anthonycaetano56@gmail.com>
2024-11-26 22:02:11 -0300

    Commit 9

--- src/Movie.java
+++ src/Movie.java
@@ -1,7 +1,6 @@
 package AulaPraticaRefactoring.src;
 
 public class Movie {
-
     public static final int  CHILDRENS = 2;
     public static final int  REGULAR = 0;
     public static final int  NEW_RELEASE = 1;
@@ -25,4 +24,24 @@ public class Movie {
     public String getTitle (){
         return _title;
     }
-  }
+
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+}
--- src/Rental.java
+++ src/Rental.java
@@ -1,7 +1,6 @@
 package AulaPraticaRefactoring.src;
 
 public class Rental {
-
    private Movie _movie;
    private int _daysRented;
 
@@ -19,23 +18,7 @@ public class Rental {
    }
 
    public double getCharge() {
-      double thisAmount = 0;
-      switch (getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (getDaysRented() > 2)
-               thisAmount += (getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (getDaysRented() > 3)
-               thisAmount += (getDaysRented() - 3) * 1.5;
-            break;
-      }
-      return thisAmount;
+      return _movie.getCharge(_daysRented);
    }
 
    public int getFrequentRenterPoints() {

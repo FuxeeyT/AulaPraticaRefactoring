589b7201dad6eb8d34fc640e861c8a47679a8106
FuxeeyT <anthonycaetano56@gmail.com>
2024-11-26 22:20:15 -0300

    Commit 14

--- src/Price.java
+++ src/Price.java
@@ -3,4 +3,8 @@ package AulaPraticaRefactoring.src;
 public abstract class Price {
    public abstract int getPriceCode();
    public abstract double getCharge(int daysRented);
+
+   public int getFrequentRenterPoints(int daysRented) {
+      return 1;
+   }
 }
